7978d22a19b33bb86e5d36641a0403e682471767
Matheus Paiva <matheuspaivah2@gmail.com>
2024-08-27 17:43:16 -0300

    feat: add use fetch average hours per day hook

---
+++ frontend-app/src/squad/hooks/useFetchAverageHoursPerDay.ts
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import { axiosClient } from '@/common/services/axios-client';
+import { averageFormatter } from '@/utils/formatter';
+
+export const useFetchAverageHoursPerDay = (
+  squadId: string,
+  initialDate: string,
+  endDate: string
+) => {
+  const fetchAverageHoursPerDayPath = `/squads/${squadId}/average-hours-spent-per-day?initialDate=${initialDate}&endDate=${endDate}`;
+  const { data, isFetching, isError, refetch, isSuccess } = useQuery({
+    queryKey: ['averageHoursPerDay'],
+    queryFn: () =>
+      axiosClient
+        .get(fetchAverageHoursPerDayPath)
+        .then((response) => response.data.averageHoursSpentPerDay),
+    refetchOnWindowFocus: false,
+    enabled: false,
+  });
+
+  const averageHoursPerDay = `${averageFormatter(data)} Horas/Dia`;
+
+  return {
+    fetchAverageHoursPerDay: refetch,
+    averageHoursPerDay: averageHoursPerDay,
+    isLoadingSquad: isFetching,
+    isErrorLoadingSquad: isError,
+    isSuccess: isSuccess,
+  };
+};

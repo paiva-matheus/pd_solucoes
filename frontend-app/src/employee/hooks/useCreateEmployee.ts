2f8f40a1cdae932b08d1daf94977d0145b79d299
Matheus Paiva <matheuspaivah2@gmail.com>
2024-08-27 11:25:49 -0300

    feat: add use create employee hook

---
+++ frontend-app/src/employee/hooks/useCreateEmployee.ts
@@ -0,0 +1,34 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { axiosClient } from '@/common/services/axios-client';
+
+type CreateEmployeeFormData = {
+  name: string;
+  estimatedHours: number;
+  squadId: number;
+};
+
+export const postEmployeePath = '/employees';
+
+export const useCreatEmployee = () => {
+  const queryClient = useQueryClient();
+
+  const createEmployeeMutation = useMutation({
+    mutationKey: ['createEmployee'],
+    mutationFn: async (createEmployeeFormData: CreateEmployeeFormData) => {
+      return await axiosClient.post(postEmployeePath, createEmployeeFormData);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['employeesSpentHours'] });
+    },
+  });
+
+  const submitNewEmployee = async (
+    createEmployeeFormData: CreateEmployeeFormData
+  ) => {
+    return await createEmployeeMutation.mutateAsync(createEmployeeFormData);
+  };
+
+  return {
+    submitNewEmployee,
+  };
+};
